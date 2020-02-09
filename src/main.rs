5e6a2f94ff4a50e709b2cb486649d9058ae422a2
samir <samir.ahrioui@hotmail.fr>
2020-02-09 19:59:14 +0100

    trouve le mot le plus long

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,120 @@
-
+#![allow(dead_code, unused)]
 /**
  * represents a state of the automaton
  */
-struct Node{
-    is_terminal: bool,
-    is_initial:bool,
-    index:u32,
-  }
-  /**
-   * constructor of Node class
-   */
-  impl Node{
-     fn new(is_initial: bool, is_terminal: bool,index:u32) -> Node {
-          Node { is_initial: is_initial, 
-                  is_terminal: is_terminal,
-                  index: index }
-      }
-      
-  }
-  /**
-   * represents a transition and its maximum number of flows
-   */
-  struct Transition{
-  
-    value:String,
-    max_transit:u32,
-    current_transit:u32,
-  }
-  
-  impl Transition{
-     fn new(value: String,max_transit:u32) -> Transition {
-          Transition { value: value, 
-                  max_transit: max_transit,
-                  current_transit: 0 }
-      }
-      fn e() -> Transition {
-          Transition { value: String::from("0"), 
-                  max_transit: 0,
-                  current_transit: 0 }
-      }
-      
-  }
-  /**
-   * Generates a list of words from a language represented by an automaton
-   */
-  struct Generator{
-    nodes:Vec<Node>,
-    transitions_matrix: Vec<Vec<Transition>>,
-    words: Vec<String>
-  }
-  impl Generator{
-    fn new(nodes:Vec<Node>,transitions_matrix: Vec<Vec<Transition>>) -> Generator {
-        Generator{
-          nodes:nodes,
-          transitions_matrix: transitions_matrix,
-          words: vec![]
-        }
-    }
-    fn listAllwords(&self,word:String, index:u32) {
-      // let mut word2 = word +"abffj";
-      // self.listAllwords(word2.to_string(),1);
-      // println!(" {}",word2);
-      // for i in &self.transitions_matrix {
-      //   for j in i {
-      //     print!(" {}",j.value);
-      //   }
-      //   println!();  
-      // }
-      
-  
-     /* thread 'main' has overflowed its stack
-  fatal runtime error: stack overflow*/
+struct State {
+  is_terminal: bool,
+  is_initial: bool,
+  index: usize,
+}
+/**
+ * constructor of State class
+ */
+impl State {
+  fn new(is_initial: bool, is_terminal: bool, index: usize) -> State {
+    State {
+      is_initial: is_initial,
+      is_terminal: is_terminal,
+      index: index,
     }
   }
+}
+/**
+ * represents a transition and its maximum number of flows
+ */
+struct Transition {
+  character: String,
+  max_transit: usize,
+  current_transit: usize,
+}
+
+impl Transition {
+  fn new(character: &str, max_transit: usize) -> Transition {
+    Transition {
+      character: String::from(character),
+      max_transit: max_transit,
+      current_transit: 0,
+    }
+  }
+  fn e() -> Transition {
+    Transition {
+      character: String::from("0"),
+      max_transit: 0,
+      current_transit: 0,
+    }
+  }
+  fn increment_current_transit(&mut self) {
+    self.current_transit += 1;
+  }
+}
+struct IncidenceMatrix{
+  nb_of_states: usize,
+  incidence_matrix: Vec<Vec<Transition>>
+}
+impl IncidenceMatrix{
+  fn new(incidence_matrix: Vec<Vec<Transition>>) -> IncidenceMatrix {
+    IncidenceMatrix {
+      nb_of_states: incidence_matrix.len(),
+      incidence_matrix: incidence_matrix
+    }
+  }
+}
+/**
+ * Generates a list of words from a language represented by an automaton
+ */
+struct Generator {
+  states: Vec<State>,
+  incidence_matrix: Vec<Vec<Transition>>,
+}
+impl Generator {
+  fn new(states: Vec<State>, incidence_matrix: Vec<Vec<Transition>>) -> Generator {
+    Generator {
+      states: states,
+      incidence_matrix: incidence_matrix,
+    }
+  }
+  fn list_all_words(&mut self,words:&mut Vec<String>,state_index:usize, current_word:&str) {
+    if self.states[state_index].is_terminal {
+      if !words.contains(&String::from(current_word)) {
+        words.push(String::from(current_word));
+        println!("words: {:?}", words);
+        return;
+      }
+    };
+    for i in 0..self.incidence_matrix[state_index].len() {
+      let mut t = &mut self.incidence_matrix[state_index][i];
+      if t.current_transit < t.max_transit {
+        t.increment_current_transit();
+        let together = format!("{}{}", current_word, t.character.as_str());
+        self.list_all_words(words,i, together.as_str());
+      }
+    }
+  }
   
-  
-  
-  /**
-   * Creation of the incidence matrix and launch of word generation
-   */
-  fn main() {
-    let mut node1 = Node::new(true,false,0);
-    let mut node2 = Node::new(false,false,1);
-    let mut node3 = Node::new(false,true,2);
-  
-    let nodes: Vec<Node> = vec![node1,node2,node3];
-    let transitions_matrix : Vec<Vec<Transition>> =
+}
+
+
+/**
+ * Creation of the incidence matrix and launch of word generation
+ */
+fn main() {
+  let mut state1 = State::new(true, false, 0);
+  let mut state2 = State::new(false, false, 1);
+  let mut state3 = State::new(false, true, 2);
+
+  let states: Vec<State> = vec![state1, state2, state3];
+  let incidence_matrix: Vec<Vec<Transition>> = vec![
+    vec![Transition::e(), Transition::new("a", 1), Transition::e()],
     vec![
-        vec![Transition::e(),Transition::new(String::from("a"),1),Transition::e()],
-        vec![Transition::e(),Transition::new(String::from("b"),2),Transition::new(String::from("c"),1)],
-        vec![Transition::e(),Transition::e(),Transition::e()]
-        ];
-  
-    let generator = Generator::new(nodes,transitions_matrix);
-    generator.listAllwords(String::from(""),0);
-  
-  
-    
-  }
+      Transition::e(),
+      Transition::new("b", 2),
+      Transition::new("c", 1),
+    ],
+    vec![Transition::e(), Transition::e(), Transition::e()],
+  ];
+
+  let mut generator = Generator::new(states, incidence_matrix);
+  let mut words:Vec<String> = vec![];
+  generator.list_all_words(&mut words,0,"");
+  println!("words: {:?}", words);
+}
