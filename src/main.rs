d6020b5327c2ccfc4008ba8d1fa122dfe3ae0d0a
samir <samir.ahrioui@hotmail.fr>
2020-02-14 21:32:38 +0100

    serialize to json

--- src/main.rs
+++ src/main.rs
@@ -49,69 +49,68 @@ impl Transition {
   }
 }
 #[derive(Debug, Clone)]
-struct IncidenceMatrix{
-  nb_of_states: usize,
-  incidence_matrix: Vec<Vec<Transition>>
+struct IncidenceMatrix {
+  matrix: Vec<Vec<Transition>>,
 }
-impl IncidenceMatrix{
+impl IncidenceMatrix {
   fn new(incidence_matrix: Vec<Vec<Transition>>) -> IncidenceMatrix {
     IncidenceMatrix {
-      nb_of_states: incidence_matrix.len(),
-      incidence_matrix: incidence_matrix
+      matrix: incidence_matrix,
     }
   }
-  // fn clone(&self) -> IncidenceMatrix {
-  //   let mut cloned_matrix:Vec<Vec<Transition>> =  vec![vec![];self.nb_of_states];
-  //   for i in 0..self.incidence_matrix.len() {
-  //     for j in 0..self.incidence_matrix[i].len() {
-  //       cloned_matrix[i][j] = self.incidence_matrix[i][j].clone();
-  //     }
-  //   }
-  //   return IncidenceMatrix {
-  //     nb_of_states: self.nb_of_states,
-  //     incidence_matrix: cloned_matrix
-  //   };
-  // }
 }
 /**
  * Generates a list of words from a language represented by an automaton
  */
 struct Generator {
-  states: Vec<State>
+  states: Vec<State>,
 }
 impl Generator {
   fn new(states: Vec<State>) -> Generator {
-    Generator {
-      states: states
-    }
+    Generator { states: states }
   }
-  fn list_all_words(&mut self,words:&mut Vec<String>,state_index:usize, current_word:&str,mut incidence_matrix:IncidenceMatrix) {
+  fn list_all_words(
+    &mut self,
+    words: &mut Vec<String>,
+    state_index: usize,
+    current_word: &str,
+    mut incidence_matrix: IncidenceMatrix,
+  ) {
     //let mut cloned_matrix = self.incidence_matrix.clone();
-   
-    println!("state: {:?}", state_index+1);
+    //println!("state: {:?}", state_index+1);
     if self.states[state_index].is_terminal {
       if !words.contains(&String::from(current_word)) {
         words.push(String::from(current_word));
-        println!("words: {:?}", words);
-        return;
+        //println!("words: {:?}", words);
+        //return;
       }
     };
-    for i in 0..incidence_matrix.incidence_matrix[state_index].len() {
-
-          let mut t = &mut incidence_matrix.incidence_matrix[state_index][i];
-          if t.current_transit < t.max_transit {
-            t.increment_current_transit();
-            let together = format!("{}{}", current_word, t.character.as_str());
-            self.list_all_words(words,i, together.as_str(),incidence_matrix.clone());
-        
-      }
-     
+    for i in 0..incidence_matrix.matrix[state_index].len() {
+      let mut t = &mut incidence_matrix.matrix[state_index][i];
+      if t.current_transit < t.max_transit {
+        t.increment_current_transit();
+        let together = format!("{}{}", current_word, t.character.as_str());
+        self.list_all_words(words, i, together.as_str(), incidence_matrix.clone());
       }
     }
   }
+}
+struct SerializableInputFromFile {
+  states: Vec<State>,
+  matrix: Vec<Vec<Transition>>,
+}
+impl SerializableInputFromFile {
+  fn new(states: Vec<State>, matrix: Vec<Vec<Transition>>) -> SerializableInputFromFile {
+    SerializableInputFromFile {
+      states: states,
+      matrix: matrix,
+    }
+  }
+  fn serialize_to_json(&self){
+    
+  }
   
-
-
+}
 
 /**
  * Creation of the incidence matrix and launch of word generation
@@ -123,17 +122,17 @@ fn main() {
 
   let states: Vec<State> = vec![state1, state2, state3];
   let matrix: Vec<Vec<Transition>> = vec![
-    vec![Transition::e(), Transition::new("a", 1), Transition::e()],
+    vec![Transition::e(), Transition::new("a", 2), Transition::e()],
     vec![
       Transition::e(),
       Transition::new("b", 2),
-      Transition::new("c", 1),
+      Transition::new("c", 2),
     ],
-    vec![Transition::e(), Transition::e(), Transition::e()],
+    vec![Transition::new("d", 1), Transition::e(), Transition::e()],
   ];
   let incidence_matrix: IncidenceMatrix = IncidenceMatrix::new(matrix);
   let mut generator = Generator::new(states);
-  let mut words:Vec<String> = vec![];
-  generator.list_all_words(&mut words,0,"",incidence_matrix);
+  let mut words: Vec<String> = vec![];
+  generator.list_all_words(&mut words, 0, "", incidence_matrix);
   println!("words: {:?}", words);
 }
