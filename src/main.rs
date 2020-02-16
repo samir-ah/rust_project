e70094284b6b5586f9a554eb8c144cb4a6d8278d
samir <samir.ahrioui@hotmail.fr>
2020-02-16 23:16:31 +0100

    match_word_path

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,7 @@ impl Generator<'_> {
     words: &mut Vec<String>,
     state_index: usize,
     current_word: &str,
-    mut incidence_matrix: Vec<Vec<Transition>>,
+    mut incidence_matrix: &mut Vec<Vec<Transition>>,
   ) {
     //let mut cloned_matrix = self.incidence_matrix.clone();
     //println!("state: {:?}", state_index+1);
@@ -84,10 +84,52 @@ impl Generator<'_> {
       let mut t = &mut incidence_matrix[state_index][i];
       if t.current_transit < t.max_transit {
         t.increment_current_transit();
-        let together = format!("{}{}", current_word, t.character.as_str());
-        self.list_all_words(words, i, together.as_str(), incidence_matrix.clone());
+        let together = format!("{}{}", current_word, t.character);
+        self.list_all_words(words, i, together.as_str(), &mut incidence_matrix.clone());
       }
     }
+  }
+
+  fn match_word_path(
+    &mut self,
+    word: &str,
+    char_index: usize,
+    state_index: usize,
+    current_word: &str,
+    incidence_matrix: &Vec<Vec<Transition>>,
+    path_in_automaton: &mut Vec<usize>,
+  ) -> bool {
+    if state_index == 0 && char_index == 0 {
+      path_in_automaton.push(state_index)
+    }
+    if self.states[state_index].is_terminal && (word.len() == current_word.len()) {
+      if current_word == word {
+        return true;
+      }
+    };
+    for index_next_state in 0..incidence_matrix[state_index].len() {
+      let t = &incidence_matrix[state_index][index_next_state];
+      if t.character != "0" {
+        let new_current_word = format!("{}{}", current_word, t.character);
+        if word.get(char_index..char_index + 1)
+          == new_current_word.as_str().get(char_index..char_index + 1)
+        {
+          path_in_automaton.push(index_next_state);
+
+          if (self.match_word_path(
+            word,
+            char_index + 1,
+            index_next_state,
+            new_current_word.as_str(),
+            incidence_matrix,
+            path_in_automaton,
+          )) {
+            return true;
+          };
+        }
+      }
+    }
+    return false;
   }
 }
 #[derive(Serialize, Deserialize)]
@@ -119,7 +161,7 @@ impl Automaton {
     return serde_json::from_str(s.as_str()).unwrap();
   }
   fn save_to_json(&self, file_name: &str) {
-    let output = serde_json::to_string_pretty(self);
+    let output = serde_json::to_string(self);
     match output {
       Ok(o) => {
         let path = Path::new(file_name);
@@ -135,6 +177,43 @@ impl Automaton {
       }
       Err(e) => println!("error parsing header: {:?}", e),
     }
+  }
+  fn save_to_dot_script(&self, file_name: &str) {
+    let path = Path::new(file_name);
+    let display = path.display();
+    let mut file = File::create(&path).expect("couldn't create file");
+    file.write_all(
+      "digraph finite_state_machine {
+  rankdir = LR;
+  node [shape = circle]; 0;\n"
+        .as_bytes(),
+    );
+    for i in &self.states {
+      if i.is_terminal {
+        file.write_all(format!("    node [shape = doublecircle]; {};\n", i.index).as_bytes());
+      }
+    }
+    file.write_all(
+      "   node [shape = plaintext];
+  \"\" -> 0 [label = \"start\"];
+  node [shape = circle];\n"
+        .as_bytes(),
+    );
+
+    for i in 0..self.matrix.len() {
+      for j in 0..self.matrix[i].len() {
+        if self.matrix[i][j].character != "0" {
+          file.write_all(
+            format!(
+              "   {}->{} [label=\"{}\"];\n",
+              i, j, self.matrix[i][j].character
+            )
+            .as_bytes(),
+          );
+        }
+      }
+    }
+    file.write_all("}".as_bytes());
   }
 }
 
@@ -159,15 +238,32 @@ fn main() {
   // ];
   //////**********constructeur à partir des variable creés***********
   //let serializable_input = Automaton::new(states, matrix);
-
   //////********sauvegarder l'automate dans un fichier au format json**********
   //serializable_input.save_to_json("output.json");
 
+
   //////************charger l'automate à partir d'un fichier json**********
-  let serializable_input = Automaton::load_from_json("input.json");
+  let mut serializable_input = Automaton::load_from_json("input.json");
+
+  serializable_input.save_to_dot_script("dotScript.txt");
 
   let mut generator = Generator::new(&serializable_input.states);
   let mut words: Vec<String> = vec![];
-  generator.list_all_words(&mut words, 0, "", serializable_input.matrix);
+  generator.list_all_words(&mut words, 0, "", &mut serializable_input.matrix);
   println!("words: {:?}", words);
+
+  let mut path_in_automaton: Vec<usize> = vec![];
+  let wanted_word = "acda";
+  if generator.match_word_path(
+    wanted_word,
+    0,
+    0,
+    "",
+    &serializable_input.matrix,
+    &mut path_in_automaton,
+  ) {
+    println!("path in automaton: {:?}", path_in_automaton);
+  } else {
+    println!("{} Not found", wanted_word);
+  }
 }
